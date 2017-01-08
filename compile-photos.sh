0fd89230d6a91a692257b7a39466801ccc8572a6
Ben Wilhelm <ben@doublebeamdesign.com>
2017-01-07 21:58:03 -0600

    Add rate and dir params to compile script

--- compile-photos.sh
+++ compile-photos.sh
@@ -1,3 +1,26 @@
 #!/bin/bash
 
-ffmpeg -y -f image2 -i ./frame_%04d.jpg -r 60 -vcodec libx264 -profile high -preset slow ./timelapse.mp4
+dir=.
+rate=60
+
+while getopts "d:r:" opt; do
+  case $opt in
+    d)
+      echo "dir=$OPTARG"
+      dir=$OPTARG
+      ;;
+    r)
+      echo "rate=$OPTARG"
+      rate=$OPTARG
+      ;;
+  esac
+done
+
+ffmpeg -y -f image2    \
+-framerate $rate       \
+-i $dir/frame_%04d.jpg \
+-r $rate               \
+-vcodec libx264        \
+-profile high          \
+-preset slow           \
+$dir/timelapse.mp4
